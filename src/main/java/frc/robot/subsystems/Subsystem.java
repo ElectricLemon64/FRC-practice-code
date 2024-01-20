28d261f758debc15a4b37362ee490a3d1b1031e0
unknown <trevor.choy.1@gmail.com>
2024-01-19 17:46:07 -0800

    First Commit

---
+++ src/main/java/frc/robot/subsystems/Subsystem.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkFlex;
+
+public class Subsystem extends SubsystemBase{
+    private CANSparkFlex m1;
+    private CANSparkFlex m2;
+}
